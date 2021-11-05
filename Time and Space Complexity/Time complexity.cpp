86e3674fa43510cd50f2a1caadfdf2a9f119bd15
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2021-11-05 18:21:24 +0200

    feat: :sparkles: add codes for space complexity

--- Time and Space Complexity/Time complexity.cpp
+++ Time and Space Complexity/Time complexity.cpp
@@ -282,37 +282,3 @@ void Ex10(int N)
         }
     }
 }
-
-// Memory Analysis
-// Check if any number is repeated:    arr[i] < N
-
-// Memory O(1) & Time O(N^2)
-bool Ex8(vector<int>& arr)
-{
-    int N = arr.size();
-    for (int i = 0; i < N; ++i) {
-        for (int j = i + 1; j < N; ++j) {
-            if (arr[i] == arr[j]) {
-                return 1;
-            }
-        }
-    }
-    return 0;
-}
-
-// Another Solution: Shows Memory & Time compromising
-// Memory O(N) & Time O(N)
-bool Ex8(vector<int>& arr)
-{
-    int N = arr.size();
-
-    vector<bool> exist(N, 0); // Allocate N location
-
-    for (int i = 0; i < N; ++i) {
-        if (exist[arr[i]] == 1) {
-            return 1;
-        }
-        exist[arr[i]] = 1;
-    }
-    return 0;
-}
