1a8747851459cc8ab60de3b54056d74938b305a7
gaserashraf <elkinggaser@gmail.com>
2023-03-18 11:32:33 +0200

    Add graph material

--- Graph Theory/2023/DFS.cpp
+++ Graph Theory/2023/DFS.cpp
@@ -2,19 +2,30 @@
 
 using namespace std;
 
+vector <bool> vis;
+vector <vector <int>> adj;
+
+void dfs(int cur)
+{
+    vis[cur] = true;
+    for (auto &i : adj[cur]) {
+        if (vis[i]) continue;
+        dfs(i);
+    }
+    cout << cur+1 << ' ';
+}
+
 int main()
 {
     int n, m; cin >> n >> m;
-    vector <vector <int>> adj_list(n);
-//    vector <bitset<101>> adj_mat(n);
-    vector <vector <bool>> adj_mat(n, vector <bool> (n, false));
+    adj.assign(n, {});
+    vis.assign(n, false);
     while(m--) {
         int u, v; cin >> u >> v;
         u--, v--;
-        adj_list[u].push_back(v);
-        adj_list[v].push_back(u);
-        adj_mat[u][v] = true;
-        adj_mat[v][u] = true;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
+    dfs(0);
     return 0;
 }
